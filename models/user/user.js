63f22e540da53073aaf9b25828a5f37fb3845cf8
Roberto Valdez <robert.tito94@gmail.com>
2020-08-25 16:12:01 -0500

    add regex expessions

--- models/user/user.js
+++ models/user/user.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 const { isAlpha, isEmail } = require('validator');
 
+const passRegex = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/g;
+const nameRegex = /^[a-zA-Z][a-zA-Z\s]*$/g
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter a name'],
-        validate: [isAlpha, 'Please just enter letters']
+        validate: [
+            (v) => {
+                const res = (nameRegex.exec(v) === null);
+                return !res;
+            },
+            'Please just enter letters'
+        ]
+        // validate: [isAlpha, 'Please just enter letters']
     },
     lastName: {
         type: String,
@@ -21,7 +31,15 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please enter a password'],
-        minlength: [8, 'Minimum password length is 8 characters']
+        minlength: [8, 'Minimum password length is 8 characters'],
+        validate: 
+        [
+            (v) => {
+                const res = (passRegex.exec(v) === null);
+                return !res;
+            },
+            'Please enter a valid password: at least 1 uppercase letter, 1 digit, 1 special character'
+        ]
     }
 })
 
