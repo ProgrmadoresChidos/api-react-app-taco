65037b9c03f0fa82013cf3df334943e01ee4c261
Roberto Valdez <robert.tito94@gmail.com>
2020-08-25 20:57:44 -0500

    test

--- models/user/user.js
+++ models/user/user.js
@@ -8,14 +8,14 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter a name'],
-        validate: [
-            (v) => {
-                const res = (nameRegex.exec(v) === null);
-                return !res;
-            },
-            'Please just enter letters'
-        ]
-        // validate: [isAlpha, 'Please just enter letters']
+        // validate: [
+        //     (v) => {
+        //         const res = (nameRegex.exec(v) === null);
+        //         return !res;
+        //     },
+        //     'Please just enter letters'
+        // ]
+        validate: [isAlpha, 'Please just enter letters']
     },
     lastName: {
         type: String,
@@ -32,14 +32,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please enter a password'],
         minlength: [8, 'Minimum password length is 8 characters'],
-        validate: 
-        [
-            (v) => {
-                const res = (passRegex.exec(v) === null);
-                return !res;
-            },
-            'Please enter a valid password: at least 1 uppercase letter, 1 digit, 1 special character'
-        ]
+        // validate:
+        // [
+        //     (v) => {
+        //         // console.log(v, passRegex.exec(v) === null)
+        //         const res = (passRegex.exec(v) === null);
+        //         return !res;
+        //     },
+        //     'Please enter a valid password: at least 1 uppercase letter, 1 digit, 1 special character'
+        // ]
     }
 })
 
