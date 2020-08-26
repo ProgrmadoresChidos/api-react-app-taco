ac07666876c639a18b58388a30c1d20a9c935fd7
Roberto Valdez <robert.tito94@gmail.com>
2020-08-25 21:46:28 -0500

    fix expressions

--- models/user/user.js
+++ models/user/user.js
@@ -2,20 +2,13 @@ const mongoose = require('mongoose');
 const { isAlpha, isEmail } = require('validator');
 
 const passRegex = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/;
-const nameRegex = /^[a-zA-Z][a-zA-Z\s]*$/g
+const nameRegex = /^[a-zA-Z][a-zA-Z\s]*$/
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter a name'],
-        // validate: [
-        //     (v) => {
-        //         const res = (nameRegex.exec(v) === null);
-        //         return !res;
-        //     },
-        //     'Please just enter letters'
-        // ]
-        validate: [isAlpha, 'Please just enter letters']
+        validate: [(v) => nameRegex.test(v), 'Please just enter letters']
     },
     lastName: {
         type: String,
@@ -34,14 +27,8 @@ const userSchema = new mongoose.Schema({
         minlength: [8, 'Minimum password length is 8 characters'],
         validate:
         {
-            validator: function (v) { return passRegex.test(v) },
-            
-            // {
-            //     // console.log(v, passRegex.exec(v) === null)
-            //     const res = (passRegex.exec(v) === null);
-            //     return !res;
-            // },
-            message:'Please enter a valid password {VALUE} : at least 1 uppercase letter, 1 digit, 1 special character'
+            validator: (v) =>  passRegex.test(v),
+            message:'Please enter a valid password: at least 1 uppercase letter, 1 digit, 1 special character'
         }
     }
 })
