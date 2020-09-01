5bc87127c56242a13f171763a651bc824e48a9f9
Roberto Valdez <robert.tito94@gmail.com>
2020-08-31 22:14:28 -0500

    clean code

--- routes/authRoutes/authRoutes.js
+++ routes/authRoutes/authRoutes.js
@@ -3,12 +3,4 @@ const authController = require('../../api/authController/authController');
 
 router.route('/signup').post(authController.signup_post);
 
-// login un post                     ** nestor
-// agregar rol al modelo             ** Roberto  
-// Middleware validar token          ** nestor
-// verificar rol en el middleware    ** nestor
-// ********* CRUD users por id
-//      user/updateProfile/:id       ** JORJAIS UPDATE
-//      user/deleteProfile/:id       ** Roberto
-
 module.exports = router;
